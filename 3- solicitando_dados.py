0a979f7d5d507ec6e1e179a1ae3ae4acbfa3aab8
jessicatmota-web <jessica.t.mota@ba.estudante.senai.br>
2026-08-21 16:28:30 -0300

    Add files via upload

---
+++ 3- solicitando_dados.py
@@ -0,0 +1,24 @@
+import os 
+
+# Limpa a terminal.
+os.system("cls")
+
+#SOLICITANDO DADOS.
+# intuput adiciona o que for digitado no terminal na variável como texto.
+nome = input("Digite seu nome: ")
+sobrenome = input("Digite seu sobrenome: ")
+
+# int() converte o que foi digitado em inteiro (números interiros).
+idade = int(input("Digite sua idade: "))
+
+# float() converte o que foi digitado em float 
+peso = float(input("Digite seu peso: "))
+altura = float(input("Digite sua altura: "))
+
+#MOSTRANDO DADOS.
+print("Nome: ", nome)
+print("Sobrenome:", sobrenome)
+print("Idade: ", idade)
+print("Peso: ", peso)
+print("Altura:", altura)
+
