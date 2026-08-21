93081f651c9b40203aeb9f8d06f35f5bbd376b6a
jessicatmota-web <jessica.t.mota@ba.estudante.senai.br>
2026-08-21 16:49:06 -0300

    Add files via upload

---
+++ 6- antescessor_sucessor.py
@@ -0,0 +1,17 @@
+import os 
+
+# Limpa o terminal
+os.system("cls")
+
+
+print("+ SOLICITANDO DADOS")
+numero = int(input("Digite um número: "))
+
+#CALCULANDO
+antecessor = numero - 1
+sucessor = numero +1
+
+print("\n= EXIBINDO DADOS")
+print("Antecessor: ", antecessor)
+print("Sucessor: ", sucessor)
+
